726d03648b40552742f61c82f636232061dfe89e
Dos <doszlmnv@gmail.com>
2022-06-23 20:45:42 +0600

    Initial Commit

---
+++ TournamentJS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TournamentJS
+//
+//  Created by Dossymkhan Zhulamanov on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
